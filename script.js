cf01d9b2efdc9078c3420c1b5ed6e04a6db43947
Samburgerman <104079001+Samburgerman@users.noreply.github.com>
2022-04-22 08:11:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+var money = 0;
+
 function click()
+{
+  money++;
+  updateDisp();
+}
+
+function updateDisp()
 {
   var elem = document.getElementById('text');
   elem.innerHTML = "<h1>Samuel is cooler than you</h1>";
